5f4d3331d95e4deb4c9cb1adbb420364bca22461
Luminyx1 <79218624+Luminyx1@users.noreply.github.com>
2023-08-06 14:35:09 -0400

    graphics/LightMapMgr: Add setCSLightMaps()

--- graphics/LightMapMgr.h
+++ graphics/LightMapMgr.h
@@ -22,6 +22,9 @@ public:
 
     void setModelLightMap(ModelNW* p_model, bool set_mdl_dl_dirty) const;
 
+    // Address: 0x024E51C8
+    void setCSLightMaps(u32 = 0);
+
 private:
     u32                     _cc[(0x2070 - 0xCC) / sizeof(u32)];
     agl::lght::LightMapMgr  mLightMapMgr;
