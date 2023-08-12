a0c86f963ba2ef9fae640dac0510c99470a57ac2
AboodXD <aboood40091@gmail.com>
2023-08-12 17:17:31 +0200

    map_obj: Add (Circle)LightMask and MaskTextureType

---
+++ map_obj/MaskDraw.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <heap/seadHeap.h>
+#include <math/seadVector.h>
+
+// TODO: Figure out where this enum belongs
+enum MaskTextureType
+{
+    cMaskTexture_Cave = 0,
+    cMaskTexture_CaveSmall,
+    cMaskTexture_Dark,
+    cMaskTexture_DeepSea,
+    cMaskTexture_FlashlightA,
+    cMaskTexture_FlashlightB,
+    cMaskTexture_DrcBlock,
+    cMaskTexture_DrcTouch,
+    cMaskTexture_UserType
+};
+
+class LightMask
+{
+public:
+    virtual bool create(sead::Heap* heap, MaskTextureType) = 0;
+    virtual bool execute() = 0;
+    virtual bool draw() = 0;
+};
+static_assert(sizeof(LightMask) == 4);
+
+class Mask;
+class Quad;
+
+class CircleLightMask : public LightMask
+{
+public:
+    CircleLightMask();
+
+    bool create(sead::Heap* heap, MaskTextureType) override;
+    bool execute() override;
+    bool draw() override;
+
+protected:
+    f32             mRadius;
+    f32             _8;
+    sead::Vector3f  mPos;
+    Mask*           mpMask;
+    Quad*           mpQuad;
+    MaskTextureType mMaskTextureType;
+    u32             _24;
+};
+static_assert(sizeof(CircleLightMask) == 0x28);
