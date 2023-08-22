86a3a04483dfea9d372e8bdb424cf2bb180b20d6
AboodXD <aboood40091@gmail.com>
2023-08-22 12:14:56 +0200

    collision/BgCollisionCheckResult: A few fixes
    * Added missing header include.
    * Added default ctor.
    * Fixed size assertion.

--- collision/BgCollisionCheckResult.h
+++ collision/BgCollisionCheckResult.h
@@ -2,12 +2,19 @@
 
 #include <collision/BgCheckUnitInfo.h>
 
+#include <math/seadVector.h>
 #include <prim/seadBitFlag.h>
 
 class BgCollision;
 
 struct BgCollisionCheckResult
 {
+    BgCollisionCheckResult()
+        : _c(0)     // Probably default constructor of Angle
+        , _10(0)    // ^^^
+    {
+    }
+
     sead::BitFlag8  hit_direction_flag;
     sead::Vector2f  _4;
     u32             _c;     // Some sort of angle
@@ -15,5 +22,6 @@ struct BgCollisionCheckResult
   //u32             _14[4 / sizeof(u32)];   // Alignment???
     BgCheckUnitInfo bg_check_data;
     BgCollision*    p_bg_collision;
+  //u32             _24[4 / sizeof(u32)];   // More alignment???
 };
-static_assert(sizeof(BgCollisionCheckResult) == 0x24);
+static_assert(sizeof(BgCollisionCheckResult) == 0x28);
