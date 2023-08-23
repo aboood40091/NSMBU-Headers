fb21cf4afcba8cf8222cc0b7b1f498613459975c
AboodXD <aboood40091@gmail.com>
2023-08-23 15:47:40 +0200

    graphics/EnvSetReader: Add addresses

--- graphics/EnvSetReader.h
+++ graphics/EnvSetReader.h
@@ -11,8 +11,10 @@ class ReadStream;
 class EnvSetReader
 {
 public:
+    // Address: 0x024DD29C
     static void read(const sead::SafeString& path, sead::Heap* heap = nullptr);
 
 private:
+    // Address: 0x024DD1C0
     static void read_(sead::ReadStream& stream, sead::Heap* heap);
 };
