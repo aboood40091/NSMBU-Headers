f4457b210576a84df6934d86650ab1c5da160804
AboodXD <aboood40091@gmail.com>
2023-08-23 14:10:02 +0200

    game/Quake: Add addresses

--- game/Quake.h
+++ game/Quake.h
@@ -5,9 +5,16 @@
 
 class Quake
 {
+    // createInstance()                             Address: 0x024C47B4
+    // deleteInstance()                             Address: Deleted
+    // sInstance                                    Address: 0x101D1660
+    // SingletonDisposer_::~SingletonDisposer_()    Address: 0x024C4F14
+    // SingletonDisposer_::sStaticDisposer          Address: 0x101D1664
+    // SingletonDisposer_::vtbl                     Address: 0x100B7CF0
     SEAD_SINGLETON_DISPOSER(Quake)
 
 public:
+    // Address: 0x024C46CC
     Quake();
 
     const sead::Vector2f& getOffset() const { return mOffset; }
