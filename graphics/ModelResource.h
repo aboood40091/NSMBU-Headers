c5652795864104e23afff73d21e82417997afad6
AboodXD <aboood40091@gmail.com>
2023-08-23 15:51:15 +0200

    graphics/ModelResource: Add addresses and loadDemo()

--- graphics/ModelResource.h
+++ graphics/ModelResource.h
@@ -9,23 +9,35 @@
 
 #include <nw/g3d/res/g3d_ResFile.h>
 
-class ModelResource : public sead::IDisposer
+class ModelResource : public sead::IDisposer    // vtbl Address: 0x100BD17C
 {
 public:
+    // Address: 0x024F5548
     ModelResource();
+    // Address: 0x024F5684
     virtual ~ModelResource();
 
+    // Address: 0x024F55D0
     void destroy();
 
+    // Address: 0x024F5C14
     void load(
         const sead::SafeString& filename, sead::Heap* heap
     );
 
+    // Address: 0x024F56F8
     void load(
         sead::ArchiveRes* archive, const sead::SafeString& filename,
         const nw::g3d::res::ResFile* tex_res_file, sead::Heap* heap
     );
 
+    // Address: 0x024F5C78
+    void loadDemo(  // (This function just calls load())
+        sead::ArchiveRes* archive, const sead::SafeString& filename,
+        const nw::g3d::res::ResFile* tex_res_file, sead::Heap* heap
+    );
+
+    // Address: 0x024F5C7C
     agl::ShaderProgramArchive* getModelShaderProgramArchive(s32 idx_model) const;
 
     nw::g3d::res::ResFile* getResFile() const
