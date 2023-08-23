b1be6950ca8062159d1828368e7cb454535c0b36
AboodXD <aboood40091@gmail.com>
2023-08-23 15:46:57 +0200

    graphics/CalcRatio: Add addresses

--- graphics/CalcRatio.h
+++ graphics/CalcRatio.h
@@ -4,12 +4,20 @@
 
 class CalcRatio
 {
+    // Actual name of this class is unknown.
+    // In NSMBW, it is m3d::calcRatio_c.
+    // However, "CalcRatio" is not consistent with the code order in NSMBU.
+
 public:
+    // Address: 0x024D7F1C
     CalcRatio();
 
+    // Address: 0x024D7FE0
     void set(f32 duration);
+    // Address: 0x024D7FB8
     void reset();
 
+    // Address: 0x024D8054
     void calc();
 
     f32 ratioA() const
@@ -28,8 +36,10 @@ public:
     }
 
 private:
+    // Address: 0x024D7F94
     void reset_();
 
+    // Address: 0x024D7F78
     static bool isZero_(f32 v);
 
 private:
