4af5911a3c2e9f3c04df003c5364fae2d12d95be
AboodXD <aboood40091@gmail.com>
2023-08-23 15:47:05 +0200

    graphics/CullViewFrustum: Add addresses

--- graphics/CullViewFrustum.h
+++ graphics/CullViewFrustum.h
@@ -28,12 +28,19 @@ class CullViewFrustum
     static_assert(sizeof(Sub) == 0x10);
 
 public:
+    // Address: 0x024D78A4
     CullViewFrustum();
 
     nw::g3d::ViewVolume& getViewVolume() { return mViewVolume; }
     const nw::g3d::ViewVolume& getViewVolume() const { return mViewVolume; }
 
+    // Address: 0x024D7910
     void update(const sead::Camera& camera, const sead::PerspectiveProjection& projection);
+
+    // Address: 0x024D7D8C
+    bool testIntersectionSphere(const sead::Vector3f& pos, f32 radius) const;   // I think
+    // Address: 0x024D7DDC
+    bool testIntersectionAABB(const sead::Vector3f& min, const sead::Vector3f& max) const;
 
 private:
     sead::UnsafeArray<Sub, 4>   _0;
