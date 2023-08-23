65071251cb7370606d1224dc6d8775ee11a0df1c
AboodXD <aboood40091@gmail.com>
2023-08-23 15:47:54 +0200

    graphics/GfxParameter: Add addresses

--- graphics/GfxParameter.h
+++ graphics/GfxParameter.h
@@ -4,14 +4,23 @@
 #include <resource/seadSharcArchiveRes.h>
 #include <utility/aglParameterIO.h>
 
-class GfxParameter
+class GfxParameter  // vtbl Address: 0x100BAD04
 {
+    // createInstance()                             Address: 0x024DF350
+    // deleteInstance()                             Address: Deleted
+    // sInstance                                    Address: 0x101D2B90
+    // SingletonDisposer_::~SingletonDisposer_()    Address: 0x024DF7C8
+    // SingletonDisposer_::sStaticDisposer          Address: 0x101D2B94
+    // SingletonDisposer_::vtbl                     Address: 0x100BACF4
     SEAD_SINGLETON_DISPOSER(GfxParameter)
 
 public:
+    // Address: 0x024DF254
     GfxParameter();
+    // Address: 0x024DF3D0
     virtual ~GfxParameter();
 
+    // Address: 0x024DF5A8
     void setDelegateForParameter(agl::utl::IParameterIO* p_param);
 
 private:
