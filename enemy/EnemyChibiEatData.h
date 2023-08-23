137fe3664f2f8aff79cf1f1c385beb252204cf85
AboodXD <aboood40091@gmail.com>
2023-08-23 13:49:02 +0200

    enemy/EnemyChibiEatData: Add addresses

--- enemy/EnemyChibiEatData.h
+++ enemy/EnemyChibiEatData.h
@@ -4,16 +4,21 @@
 
 class Enemy;
 
-class EnemyChibiEatData : public GenericChibiEatData
+class EnemyChibiEatData : public GenericChibiEatData    // vtbl Address: 0x10072738
 {
 public:
+    // Address: 0x0232ADAC
     EnemyChibiEatData(ActorUniqueID owner_id);
 
+    // Address: 0x0232AEAC
     u32 vf0C() override;
+    // Address: 0x0232AFB0
     void* vf14(void*) override;
+    // Address: 0x0232B01C
     void vf2C() override;
 
 protected:
+    // Address: 0x0232AE14
     Enemy* getOwner_() const;
 };
 static_assert(sizeof(EnemyChibiEatData) == sizeof(GenericChibiEatData));
