b8db00c67e39c8b71abcb721a923c56b34077204
AboodXD <aboood40091@gmail.com>
2023-09-23 03:45:04 +0400

    system/ApplicationFramework: Add remaining functions
    And correct a few of the attributes.

--- system/ApplicationFramework.h
+++ system/ApplicationFramework.h
@@ -9,23 +9,34 @@ class ApplicationFramework : public sead::GameFrameworkCafe
     SEAD_RTTI_OVERRIDE(ApplicationFramework, sead::GameFrameworkCafe)
 
 public:
+    static u32 toSeconds(u32 hours);
+    static u32 toHours(u32 seconds);
+    static u32 getTimeBeforeAPD();  // In hours, APD = Auto Power Down
+    static void setDimEnable(bool enable);
+    static bool isDimEnable();
+
+public:
     ApplicationFramework(const CreateArg& arg);
 
-    virtual void run(sead::Heap*, const sead::TaskBase::CreateArg&, const RunArg&);
-    virtual void createProcessMeter(sead::TaskBase*);
+    void run(sead::Heap* heap, const sead::TaskBase::CreateArg& root_create_arg, const RunArg& run_arg) override;
+    void createProcessMeter(sead::TaskBase* root_task) override;
 
 protected:
-    virtual void waitStartDisplayLoop_();
-    virtual void procDraw_();
-    virtual void procCalc_();
-    virtual void swapBuffer_();
+    void procDraw_() override;
+
+    void calc_();
+    void procCalc_() override;
+
+    void swapBuffer_() override;
+
+    void waitStartDisplayLoop_() override;
 
 protected:
-    sead::ProcessMeterBarBase   _378;   // sead::MultiProcessMeterBar<???>
-    sead::ProcessMeterBarBase   _380;   // sead::MultiProcessMeterBar<???>
-    u8                          _388;
-    u32                         _38C;
-    u32                         _390;
-    ProcUIStatus                mProcUIStatus;
+    sead::MultiProcessMeterBar<1>   mLayerMgrCalcMeter; // sead::MultiProcessMeterBar<???>
+    sead::MultiProcessMeterBar<1>   _380;               // sead::MultiProcessMeterBar<???>
+    bool                            mFirstSwapDone;
+    s32                             _38C;
+    s32                             _390;
+    ProcUIStatus                    mProcUIStatus;
 };
 static_assert(sizeof(ApplicationFramework) == 0x398);
