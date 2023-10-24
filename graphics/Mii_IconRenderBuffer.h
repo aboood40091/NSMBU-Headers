d7fb20e04fb244d7d0763ec2d4b8ee218fb25896
AboodXD <aboood40091@gmail.com>
2023-10-24 12:40:16 +0400

    graphics/Mii: Add IconRenderBuffer

---
+++ graphics/Mii_IconRenderBuffer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <graphics/CaptureRenderBuffer.h>
+
+class ModelFFL;
+
+namespace Mii {
+
+class IconRenderBuffer : public sead::IDisposer, public CaptureRenderBuffer
+{
+public:
+    IconRenderBuffer()
+    {
+    }
+
+    virtual ~IconRenderBuffer()
+    {
+        destroy();
+    }
+
+    // Address: 0x024E9708
+    void initialize(sead::Heap* heap, const TextureInfo& info);
+    // Address: 0x024E9904
+    void destroy();
+
+    // Address: 0x024E9868
+    bool doesNothing(const ModelFFL* p_model);
+};
+static_assert(sizeof(IconRenderBuffer) == 0x460);
+
+}
