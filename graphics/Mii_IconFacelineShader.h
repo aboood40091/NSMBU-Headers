26d85a9f49aeb321ea4553fdd1a13a0d82089b3c
AboodXD <aboood40091@gmail.com>
2023-10-24 12:40:45 +0400

    graphics/Mii/IconFacelineShader: Declare more functions

--- graphics/Mii_IconFacelineShader.h
+++ graphics/Mii_IconFacelineShader.h
@@ -4,11 +4,19 @@
 #include <common/aglTextureSampler.h>
 #include <container/seadSafeArray.h>
 #include <heap/seadHeap.h>
+#include <math/seadMatrix.h>
 
 namespace Mii {
 
 class IconFacelineShader
 {
+public:
+    enum Sampler
+    {
+        cSampler_Depth = 0,
+        cSampler_Color
+    };
+
 public:
     // Address: 0x024E9078
     IconFacelineShader();
@@ -25,6 +33,21 @@ public:
     bool initialize();
     // Address: 0x024E9704
     void destroy();
+
+    void activate() const
+    {
+        mpShaderProgram->activate();
+    }
+
+    // Address: 0x024E92C4
+    void setViewUniform(const sead::Matrix34f& view_mtx, const sead::Matrix44f& proj_mtx, const sead::Matrix34f& world_mtx) const;
+    // Address: 0x024E93CC
+    void setResolutionUniform(u32 width, u32 height) const;
+    // Address: 0x024E94E8
+    void setLineWidthUniform(s32 line_width) const;
+
+    // Address: 0x024E9580
+    void activateTexture(const agl::TextureData& texture_data, Sampler sampler);
 
 private:
     agl::ShaderProgramArchive*              mpShaderArchive;
