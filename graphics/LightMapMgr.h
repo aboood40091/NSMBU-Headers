28806aab4086a9060be4786221c2a97582319508
AboodXD <aboood40091@gmail.com>
2024-01-02 06:13:06 +0400

    graphics/LightMapMgr: Define members

--- graphics/LightMapMgr.h
+++ graphics/LightMapMgr.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <graphics/RenderObj.h>
+
+#include <container/seadSafeArray.h>
+#include <environment/aglEnvObjMgr.h>
 #include <framework/seadCalculateTask.h>
 #include <framework/seadTaskMgr.h>
+#include <gfx/seadCamera.h>
+#include <gfx/seadProjection.h>
+#include <layer/aglDrawMethod.h>
 #include <lighting/aglLightMapMgr.h>
+
+#include <nw/g3d/res/g3d_ResTexture.h>
 
 class ModelG3d;
 
@@ -30,6 +39,34 @@ public:
     };
     static_assert(cEnvTypeCourse_Num == 7);
 
+    enum AmbColorType
+    {
+        cAmbColorType_EffectColor13_0 = 0,
+        cAmbColorType_EffectColor13_1,
+        cAmbColorType_EffectColor14_0,
+        cAmbColorType_EffectColor14_1,
+        cAmbColorType_EffectColor15_0,
+        cAmbColorType_EffectColor15_1,
+        AmbColorType_WaterHalfFrontColor0,
+        AmbColorType_WaterHalfFrontKonst0,
+        AmbColorType_WaterHalfFrontKonst1,
+        AmbColorType_WaterHalfBehindColor0,
+        AmbColorType_WaterHalfBehindColor1,
+        AmbColorType_Num
+    };
+    static_assert(AmbColorType_Num == 11);
+
+private:
+    class CalcObj : public RenderObjBase
+    {
+    public:
+        void calc() override;
+
+    private:
+        agl::lght::LightMapMgr* mpLightMapMgr;
+    };
+    static_assert(sizeof(CalcObj) == 8);
+
 public:
     // Address: 0x024E3A98
     LightMapMgr(const sead::TaskConstructArg& arg);
@@ -54,9 +91,34 @@ public:
     const agl::TextureSampler& getLightmap2(EnvTypeCourse index) const;
 
 private:
-    u32                     _cc[(0x2070 - 0xCC) / sizeof(u32)];
-    agl::lght::LightMapMgr  mLightMapMgr;
-    u32                     _4ddc[(0x52C8 - 0x4DDC) / sizeof(u32)];
-    bool                    mEnable;
+    u32                         _cc[4 / sizeof(u32)];
+    nw::g3d::res::ResTexture*   mpResTexture[40];
+    nw::g3d::res::ResTexture    mResTexture[40];
+    agl::lyr::DrawMethod        mDrawMethodMain;
+    agl::lyr::DrawMethod        mDrawMethodMainDRC;
+    agl::lyr::DrawMethod        mDrawMethodReflection;
+    agl::lyr::DrawMethod        mDrawMethodReflectionDRC;
+    agl::lght::LightMapMgr      mLightMapMgr;
+    CalcObj                     mCalcObj;
+    agl::env::EnvObjMgr         mEnvObjMgr;
+    bool                        mIsViewDependent;
+    sead::Camera*               mpCamera;
+    sead::Projection*           mpProjection;
+    sead::Camera*               mpCameraDRC;
+    sead::Projection*           mpProjectionDRC;
+    sead::SafeArray<
+        sead::SafeArray<
+            s32,
+            cEnvTypeCourse_Num
+        >,
+        cLightMapNum
+    >                           mLightMapIndexCourse;
+    sead::SafeArray<
+        s32,
+        AmbColorType_Num
+    >                           mAmbientLightTypeIndex;
+    const void*                 mpAglEnvBinary1;
+    const void*                 mpAglEnvBinary2;
+    bool                        mEnable;
 };
 static_assert(sizeof(LightMapMgr) == 0x52CC);
