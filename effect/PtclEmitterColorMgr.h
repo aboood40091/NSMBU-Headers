18749abd869ec6ae6e7753fcd02d4c00184b3aba
AboodXD <aboood40091@gmail.com>
2024-01-23 09:17:03 +0400

    effect/PtclEmitterColorMgr: Define color max count as a constant

--- effect/PtclEmitterColorMgr.h
+++ effect/PtclEmitterColorMgr.h
@@ -12,6 +12,8 @@ class EmitterController;
 
 class PtclEmitterColorMgr : public sead::IDisposer // vtbl Address: 0x10058888
 {
+    static const s32 cColorMax = 6;
+
 public:
     // Address: 0x022ABFC0
     PtclEmitterColorMgr();
@@ -20,9 +22,9 @@ public:
     // Address: 0x022AC078
     bool updateAmbientLight(bool update_emitter = true);
     // Address: 0x022AC160
-    void setEmitterColor(nw::eft::EmitterController* p_emitter_controller, u16 user_data);
+    void setEmitterColor(nw::eft::EmitterController* p_emitter_controller, u16 user_data_bit);
 
 private:
-    sead::UnsafeArray<sead::Color4f, 6> mLightColor;
+    sead::UnsafeArray<sead::Color4f, cColorMax> mLightColor;
 };
 static_assert(sizeof(PtclEmitterColorMgr) == 0x70);
