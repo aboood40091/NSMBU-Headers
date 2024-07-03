0b4c52f084a7ca377a588e87107f12e4ac5c9c10
AboodXD <aboood40091@gmail.com>
2024-07-03 07:03:16 +0400

    player: Slightly update PlayerBase & PlayerObject

--- player/PlayerBase.h
+++ player/PlayerBase.h
@@ -12,7 +12,12 @@ public:
     }
 
 protected:
-    u8          _27c[0x2A8 - 0x27C];
+    u32         _27c[(0x2A8 - 0x27C) / sizeof(u32)];
     PlayerKey   mPlayerKey;
-    // ...
+    u32         _33c[(0x490 - 0x33C) / sizeof(u32)];
+    u32         mStatusArray[9];
+    u32         _4b4[(0x1ACC - 0x4B4) / sizeof(u32)];
+    u32         _1acc;
+    u32         _1ad0[(0x2750 - 0x1AD0) / sizeof(u32)];
 };
+static_assert(sizeof(PlayerBase) == 0x2750);
--- player/PlayerObject.h
+++ player/PlayerObject.h
@@ -5,8 +5,17 @@
 class PlayerObject : public PlayerBase
 {
 public:
+    // ...
     bool vf8CC(Actor*, u32);
     bool setDamageState(Actor*, u32); // 8D4
-
     // ...
+
+    s32 getJumpFallType();
+
+protected:
+    u32 _2750[(0x34CC - 0x2750) / sizeof(u32)];
+    f32 mJumpTimerF;
+    s32 mJumpAnmID;
+    s32 _34d4;  // Some random timer
 };
+static_assert(sizeof(PlayerObject) == 0x34D8);
