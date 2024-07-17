7d503fb36976fca6004e4a96fee7e60203993bb1
AboodXD <aboood40091@gmail.com>
2024-07-17 18:55:05 +0400

    effect/PtclCalcCallback: Add function addresses

--- effect/PtclCalcCallback.h
+++ effect/PtclCalcCallback.h
@@ -5,7 +5,10 @@
 class PtclCalcCallback
 {
 public:
+    // Address: 0x022ABA98
     static void callback_0(nw::eft::ParticleCalcArg& arg);
+    // Address: 0x022ABB3C
     static void callback_1(nw::eft::ParticleCalcArg& arg);
+    // Address: 0x022ABD34
     static void callback_2(nw::eft::ParticleCalcArg& arg);
 };
