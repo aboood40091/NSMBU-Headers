51a2d5b73a400894b70f8def4938b6184076da72
AboodXD <aboood40091@gmail.com>
2024-07-18 00:12:22 +0400

    effect/PtclEmitterUserDataUtil: Fix function declarations

--- effect/PtclEmitterUserDataUtil.h
+++ effect/PtclEmitterUserDataUtil.h
@@ -11,10 +11,10 @@ public:
     // Address: 0x022AC4C0
     static bool isForceCalcEmitterSet(u16 user_data_bit);
     // Address: 0x022AC4C8
-    static u8 getEmitterSetGroupID(u16 user_data_bit);
+    static s32 getEmitterSetGroupID(u16 user_data_bit, s32 player_no);
 
     // Address: 0x022AC508
     static u32 getEmitterUserType(u16 user_data_bit);
     // Address: 0x022AC604
-    static u32 getEmitterColorType(u16 user_data_bit);
+    static s32 getEmitterColorType(u16 user_data_bit);
 };
