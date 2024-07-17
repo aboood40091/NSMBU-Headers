65334716b8b67f8b3d39fe63a34bfd3e731d64f0
AboodXD <aboood40091@gmail.com>
2024-07-17 18:53:26 +0400

    effect: Add PtclCalcCallback

---
+++ effect/PtclCalcCallback.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <ptcl/seadPtclSystem.h>
+
+class PtclCalcCallback
+{
+public:
+    static void callback_0(nw::eft::ParticleCalcArg& arg);
+    static void callback_1(nw::eft::ParticleCalcArg& arg);
+    static void callback_2(nw::eft::ParticleCalcArg& arg);
+};
