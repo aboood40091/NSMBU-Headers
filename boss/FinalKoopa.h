a5a3d9ec4594f4374b8cb1b21b41b32e42743ece
AboodXD <aboood40091@gmail.com>
2024-07-20 02:51:05 +0400

    boss: Add FinalKoopa

---
+++ boss/FinalKoopa.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <ptcl/seadPtclSystem.h>
+
+class FinalKoopa // : public Boss
+{
+public:
+    // Address: 0x0208E268
+    static void emitterPostCalcCallback(nw::eft::EmitterPostCalcArg& arg);
+};
