ff13485b9aa5d867f6bfd9830f236acd26bbd589
AboodXD <aboood40091@gmail.com>
2024-07-20 02:50:40 +0400

    effect/PtclParallelExecuter: Complete definition

--- effect/PtclParallelExecuter.h
+++ effect/PtclParallelExecuter.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <container/seadPtrArray.h>
 #include <heap/seadDisposer.h>
 #include <ptcl/seadPtclSystem.h>
 #include <thread/seadAtomic.h>
+#include <thread/seadMessageQueue.h>
 
 class ParallelExecuter;
 class PtclMgr;
@@ -13,9 +15,29 @@ protected:
     static const s32 cExecuterNum = 2;
     static const s32 cEmitterMaxNum = 256;
 
+    enum Step
+    {
+        cStep_Ptcl = 1,
+        cStep_ChildPtcl
+    };
+
+    // Address: 0x022AECD0
+    static s32 comparePtcl_(const nw::eft::EmitterInstance* a, const nw::eft::EmitterInstance* b);
+    // Address: 0x022AECE0
+    static s32 compareChildPtcl_(const nw::eft::EmitterInstance* a, const nw::eft::EmitterInstance* b);
+
+    // Address: 0x022AECF0
+    void run_(Step step);
+
+    // Address: 0x022AEE94
+    void runCallback_(sead::Thread*, sead::MessageQueue::Element step);
+
 public:
     // Address: 0x022AF060
     PtclParallelExecuter(sead::Heap* heap, PtclMgr* p_mgr, sead::ptcl::PtclSystem* p_system);
+
+    // Address: 0x022AF420
+    void execute();
 
 protected:
     sead::ptcl::PtclSystem* mpPtclSystem;
