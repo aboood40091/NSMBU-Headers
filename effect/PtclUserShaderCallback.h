f042f1346ec63ee7cffe8c35340c629be9a50f18
AboodXD <aboood40091@gmail.com>
2024-07-21 02:17:08 +0400

    effect/PtclUserShaderCallback: Complete definition

--- effect/PtclUserShaderCallback.h
+++ effect/PtclUserShaderCallback.h
@@ -4,6 +4,36 @@
 
 class PtclUserShaderCallback
 {
+protected:
+    enum AreaLoopShaderParam
+    {
+        cAreaLoopShaderParam_RepeatNum      = 0,
+        cAreaLoopShaderParam_BoxScaleX,
+        cAreaLoopShaderParam_BoxScaleY,
+        cAreaLoopShaderParam_BoxScaleZ,
+        cAreaLoopShaderParam_BoxAlphaRatioX,
+        cAreaLoopShaderParam_BoxAlphaRatioY,
+        cAreaLoopShaderParam_BoxAlphaRatioZ
+    };
+
+    struct AreaLoopShaderVtxUniform
+    {
+        sead::Vector4f  vec0;
+        sead::Vector4f  vec1;
+        sead::Vector4f  vec2;
+        sead::Vector4f  vec3;
+        sead::Vector4f  vec4;
+    };
+    static_assert(sizeof(AreaLoopShaderVtxUniform) == 0x50);
+
+    // Address: 0x022AF678
+    static void setAreaLoopRepeatNum_(s32 data_idx, s32 rep_num);
+    // Address: 0x022AF68C
+    static s32 getAreaLoopRepeatNum_(s32 data_idx);
+
+    // Address: 0x022AF6A0
+    static f32 getRand_(u32& seed);
+
 public:
     // Address: 0x022AF710
     static void areaLoopDrawOverrideCallback(nw::eft::ShaderDrawOverrideArg& arg);
