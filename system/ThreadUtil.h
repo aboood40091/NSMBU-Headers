73787aab2db7d7a28411e19ece0dc451c85cf9f0
AboodXD <aboood40091@gmail.com>
2024-07-20 22:01:17 +0400

    system: Add ThreadUtil

---
+++ system/ThreadUtil.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <thread/seadThread.h>
+#include <thread/seadThreadUtil.h>
+
+class ThreadUtil
+{
+public:
+    enum SeadPriority
+    {
+        cSeadPriority_ParallelExecuter = sead::Thread::cDefaultSeadPriority + 1,
+        cSeadPriority_CourseSelectParallelExecuter,
+        cSeadPriority_SaveMgr,
+        cSeadPriority_Prepare,
+        cSeadPriority_CourseCacheMgr,
+        cSeadPriority_BackgroundLoadMgr,
+        cSeadPriority_Unused,
+        cSeadPriority_Olive,
+        cSeadPriority_CommentMgr
+    };
+
+public:
+    static s32 getPriority(SeadPriority prio)
+    {
+        return sead::ThreadUtil::ConvertPrioritySeadToPlatform(prio);
+    }
+};
