7bf7c2d16b94a479faed99b4d3616a0b5b8002f0
AboodXD <aboood40091@gmail.com>
2024-08-06 11:46:24 +0400

    layout_sys/TagProcessor: Add virtual functions

--- layout_sys/TagProcessor.h
+++ layout_sys/TagProcessor.h
@@ -5,13 +5,18 @@
 #include <nw/font/font_TagProcessorBase.h>
 #include <nw/lyt/lyt_TextBox.h>
 
-class TagProcessor : public nw::lyt::TextBox::TagProcessor
+class TagProcessor : public nw::lyt::TextBox::TagProcessor  // vtbl Address: 0x100E4580
 {
 public:
     TagProcessor()
         : mFontSize(1.0f)
     {
     }
+
+    // Address: 0x0267C0C4
+    virtual Operation Process(u16 code, ContextType* pContext);
+    // Address: 0x0267C3FC
+    virtual Operation CalcRect(nw::ut::Rect* pRect, u16 code, ContextType* pContext);
 
 protected:
     f32 mFontSize;
