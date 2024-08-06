d3a07f61abd9be2d58997a58913799905c14614b
AboodXD <aboood40091@gmail.com>
2024-08-06 11:53:03 +0400

    layout: Add ChangePlayerNum

---
+++ layout/ChangePlayerNum.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <layout_sys/AnimLayout.h>
+#include <state/FStateMgr.h>
+
+#include <layer/aglRenderInfo.h>
+
+class ChangePlayerNum   // vtbl Address: 0x100C8CE0
+{
+public:
+    // Address: 0x02553AB8
+    ChangePlayerNum();
+    // Address: 0x02553D24
+    virtual ~ChangePlayerNum();
+
+    // Address: 0x02553E28
+    void create();
+
+    // Address: 0x025546B8
+    void execute();
+
+    // Address: 0x025548D0
+    void draw(const agl::lyr::RenderInfo& render_info) const;
+    // Address: 0x0255491C
+    void drawBtn(const agl::lyr::RenderInfo& render_info) const;
+
+    // Address: 0x025549A8
+    void gather();
+
+    // Address: 0x02554A24
+    void setPlayerCharacter(s32 character, s32 player_no);
+    // Address: 0x02554A70
+    void resetPlayerCharacter(s32 character, s32 player_no);
+
+    void open()
+    {
+        mIsOpen = true;
+    }
+
+    bool isOpen() const
+    {
+        return mIsVisible;
+    }
+
+protected:
+    FStateMgr<ChangePlayerNum>  mStateMgr;
+    AnimLayout<1>               mLayout;
+    u32                         _ec4[(0xB798 - 0xEC4) / sizeof(u32)];
+    bool                        mIsOpen;
+    u8                          _b799;
+    bool                        mIsVisible;
+    u32                         _b79c[(0xB7A0 - 0xB79C) / sizeof(u32)];
+};
+static_assert(sizeof(ChangePlayerNum) == 0xB7A4);
