af83e9cf9dfb6eef2f44756cae805442e9bf5dbc
AboodXD <aboood40091@gmail.com>
2024-10-24 20:04:17 +0400

    sound/SndAudioMgr: Add getSoundArchive()

--- sound/SndAudioMgr.h
+++ sound/SndAudioMgr.h
@@ -4,7 +4,13 @@
 
 #include <heap/seadDisposer.h>
 
+#include <nw/snd/snd_SoundArchive.h>
+
 class SndAudioMgr : public AudAudioMgr
 {
     SEAD_SINGLETON_DISPOSER(SndAudioMgr)
+
+public:
+    // Address: 0x029B3BA8
+    nw::snd::SoundArchive* getSoundArchive();
 };
