2b105ff10111f1ce0f95d90d9ca35b56b1716487
AboodXD <aboood40091@gmail.com>
2024-10-31 13:07:12 +0400

    enemy: Add MechaKoopa

---
+++ enemy/MechaKoopa.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include <actor/Profile.h>
+#include <collision/ActorCollisionHitCallback.h>
+#include <enemy/CarryEnemy.h>
+#include <enemy/EnemyActorScaler.h>
+
+class MechaKoopaCB : public ActorCollisionHitCallback   // vtbl Address: 0x100945D4
+{
+public:
+    // Address: 0x023E8F9C
+    bool ccCallback1(ActorCollisionCheck*, const sead::Vector2f&) override;
+    // Address: 0x023E8FA4
+    void ccCallback2(ActorCollisionCheck*, const sead::Vector2f&) override;
+};
+static_assert(sizeof(MechaKoopaCB) == sizeof(ActorCollisionHitCallback));
+
+class BasicModel;
+class ModelResource;
+
+class MechaKoopa : public CarryEnemy    // vtbl Address: 0x10094064
+{
+    // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101EAB30
+    // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101EAB34
+    SEAD_RTTI_OVERRIDE(MechaKoopa, CarryEnemy)
+
+public:
+    // Address: 0x10093DD4
+    static const ActorCreateInfo cActorCreateInfo;
+
+public:
+    // Address: 0x023E8A00
+    MechaKoopa(const ActorCreateParam& param);
+
+protected:
+    // Address: 0x023E92E0
+    s32 create_() override;
+    // Address: 0x023E95CC
+    s32 execute_() override;
+    // Address: 0x023E9768
+    s32 draw_() override;
+
+public:
+    // Address: 0x023EB8C8
+    bool isSpinLiftUpEnable() override;
+    // Address: 0x023EA364
+    void splashWater(sead::Vector3f*) override;
+    // Address: 0x023EA378
+    void splashLava(sead::Vector3f*) override;
+    // Address: 0x023EA38C
+    void splashPoison(sead::Vector3f*) override;
+    // Address: 0x023EA184
+    void blockHitInit() override;
+
+    // Address: 0x023EA3A0
+    bool vf18C() override;
+    // Address: 0x023EA058
+    bool createIceActor() override;
+    // Address: 0x023EA104
+    void setIceAnm() override;
+    // Address: 0x023EBAF4
+    void calcMdl() override;
+    // Address: 0x023E9E90
+    bool etcDamageCheck(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023E99FC
+    void vsEnemyHitCheck_Normal(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023E9DD4
+    bool hitCallback_Spin(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023E9B8C
+    bool hitCallback_HipAttk(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023E9D6C
+    bool hitCallback_Fire(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+
+    // StateID_DieFall          Address: 0x10204D70
+    // initializeState_DieFall  Address: 0x023EAEF0
+    // executeState_DieFall     Address: 0x023EAF28
+    // finalizeState_DieFall    Address: 0x023EAF64
+    DECLARE_STATE_VIRTUAL_ID_OVERRIDE(MechaKoopa, DieFall)
+
+    // Address: 0x023E979C
+    void reactFumiProc(Actor*) override;
+    // Address: 0x023E98C4
+    void reactSpinFumiProc(Actor*) override;
+    // Address: 0x023E98D4
+    void playerHitCheck_Awake(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023E9990
+    void playerHitCheck_Sleep(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023EA000
+    void playerHitCheck_Unknown(ActorCollisionCheck* cc_self, ActorCollisionCheck* cc_other) override;
+    // Address: 0x023EA4B0
+    void setDropChangeState(Actor*) override;
+    // Address: 0x023EA4C8
+    void setThrowChangeState(Actor*) override;
+
+    // StateID_Carry            Address: 0x10204D4C
+    // initializeState_Carry    Address: 0x023EA834
+    // executeState_Carry       Address: 0x023EA8A0
+    // finalizeState_Carry      Address: 0x023EA99C
+    DECLARE_STATE_VIRTUAL_ID_OVERRIDE(MechaKoopa, Carry)
+    // StateID_Sleep            Address: 0x10204D28
+    // initializeState_Sleep    Address: 0x023EA78C
+    // executeState_Sleep       Address: 0x023EA7B4
+    // finalizeState_Sleep      Address: 0x023EA824
+    DECLARE_STATE_VIRTUAL_ID_OVERRIDE(MechaKoopa, Sleep)
+
+    // StateID_Walk         Address: 0x10204C94
+    // initializeState_Walk Address: 0x023EA4E0
+    // executeState_Walk    Address: 0x023EA56C
+    // finalizeState_Walk   Address: 0x023EBAF8
+    DECLARE_STATE_ID(MechaKoopa, Walk)
+    // StateID_Turn         Address: 0x10204CB4
+    // initializeState_Turn Address: 0x023EA6C8
+    // executeState_Turn    Address: 0x023EA6E4
+    // finalizeState_Turn   Address: 0x023EBAFC
+    DECLARE_STATE_ID(MechaKoopa, Turn)
+    // StateID_Slide            Address: 0x10204CD4
+    // initializeState_Slide    Address: 0x023EA9DC
+    // executeState_Slide       Address: 0x023EAC0C
+    // finalizeState_Slide      Address: 0x023EAD6C
+    DECLARE_STATE_ID(MechaKoopa, Slide)
+    // StateID_Recover          Address: 0x10204CF4
+    // initializeState_Recover  Address: 0x023EADC4
+    // executeState_Recover     Address: 0x023EAE54
+    // finalizeState_Recover    Address: 0x023EAEE0
+    DECLARE_STATE_ID(MechaKoopa, Recover)
+
+protected:
+    ModelResource*      mpModelResource;
+    BasicModel*         mpBasicModel;
+    u32                 mNextTurnTimer;
+    u32                 mDownTimer;
+    u32                 mCurrentAnim;
+    u8                  _18cc;
+    u8                  _18cd;
+    MechaKoopaCB        mCollisionHitCallback;
+    EnemyActorScaler    mScaler;
+    f32                 _18f4;
+};
+static_assert(sizeof(MechaKoopa) == 0x18F8);
