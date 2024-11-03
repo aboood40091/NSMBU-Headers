a57bd4ff6538f901baf723ecc743333816550b41
AboodXD <aboood40091@gmail.com>
2024-11-03 18:08:20 +0400

    map/WaterType: Label types

--- map/WaterType.h
+++ map/WaterType.h
@@ -5,11 +5,11 @@
 enum WaterType
 {
     cWaterType_None = 0,
-    cWaterType_1,
-    cWaterType_2,
-    cWaterType_3,
-    cWaterType_4,
-    cWaterType_5,
+    cWaterType_Water_1,
+    cWaterType_Water_2,
+    cWaterType_Lava,
+    cWaterType_LavaWave,
+    cWaterType_Poison,
     cWaterType_Quicksand
 };
 static_assert(sizeof(WaterType) == 4);
