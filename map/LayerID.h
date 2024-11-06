bed9e60370778c2c53b15741577a856ba016f1e7
AboodXD <aboood40091@gmail.com>
2024-11-06 07:13:50 +0400

    map/LayerID: Add maximum for map data

--- map/LayerID.h
+++ map/LayerID.h
@@ -7,6 +7,7 @@ enum LayerID
     cLayerID_Layer1 = 0,
     cLayerID_Layer2,
     cLayerID_Layer0,
+    cLayerID_Map_Max,
 
-    cLayerID_Layer0_Actor
+    cLayerID_Layer0_Actor = cLayerID_Map_Max
 };
