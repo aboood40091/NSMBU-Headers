1eed748f4738d2273327981b1c6631c90a4dd874
AboodXD <aboood40091@gmail.com>
2024-12-27 18:44:13 +0400

    sound/SndObjectEnemy: Add SndObjctEmy

--- sound/SndObjectEnemy.h
+++ sound/SndObjectEnemy.h
@@ -2,6 +2,31 @@
 
 #include <sound/SndObjectCommon.h>
 
+class SndObjctEmy : public NMSndObject
+{
+public:
+    SndObjctEmy(ObjType type, nw::snd::OutputLine line_flag)
+        : NMSndObject(type, line_flag)
+    {
+    }
+
+    // Address: 0x029BE940
+    void startSound(const char* label, nw::snd::OutputLine line_flag) override;
+    // Address: 0x029BE9E8
+    void holdSound(const char* label, nw::snd::OutputLine line_flag) override;
+    
+    // Address: 0x029BE9EC
+    void startSound(const char* label, s16 seq_var, nw::snd::OutputLine line_flag) override;
+    // Address: 0x029BEA9C
+    void holdSound(const char* label, s16 seq_var, nw::snd::OutputLine line_flag) override;
+    
+    // Address: 0x029BEAA0
+    void startSound(const char* label, const sead::Vector2f& pos, nw::snd::OutputLine line_flag) override;
+    // Address: 0x029BEAA4
+    void holdSound(const char* label, const sead::Vector2f& pos, nw::snd::OutputLine line_flag) override;
+};
+static_assert(sizeof(SndObjctEmy) == sizeof(NMSndObject));
+
 class SndObjctCmnEmy : public NMSndObjectCmn
 {
 public:
