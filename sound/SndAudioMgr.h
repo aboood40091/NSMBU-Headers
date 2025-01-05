f44d13ca99745a863327b1c7afa3a81bb96bcac9
AboodXD <aboood40091@gmail.com>
2025-01-05 09:48:10 +0400

    sound/SndAudioMgr: Add startDrcTouchSound()

--- sound/SndAudioMgr.h
+++ sound/SndAudioMgr.h
@@ -19,4 +19,7 @@ public:
 
     // Address: 0x029B5434
     bool startSound(nw::snd::SoundHandle* p_handle, const char* label, nw::snd::OutputLine line_flag = nw::snd::OUTPUT_LINE_MAIN);
+
+    // Address: 0x029B5934
+    void startDrcTouchSound(const sead::Vector2f& world_pos, u32 type);
 };
