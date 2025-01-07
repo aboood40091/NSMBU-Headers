9679caea5b4c88ff67836a9545cc794efee4c2f7
AboodXD <aboood40091@gmail.com>
2025-01-08 00:51:54 +0400

    player/PlayerBase: Add first state manager attribute

--- player/PlayerBase.h
+++ player/PlayerBase.h
@@ -2,6 +2,7 @@
 
 #include <actor/Actor.h>
 #include <player/PlayerKey.h>
+#include <state/FStateMgr.h>
 
 class PlayerBase : public Actor // vtbl Address: 0x10166E84
 {
@@ -44,12 +45,14 @@ public:
     bool isTotten() const;
 
 protected:
-    u32         _27c[(0x2A8 - 0x27C) / sizeof(u32)];
-    PlayerKey   mPlayerKey;
-    u32         _33c[(0x490 - 0x33C) / sizeof(u32)];
-    u32         mStatusArray[9];
-    u32         _4b4[(0x1ACC - 0x4B4) / sizeof(u32)];
-    u32         _1acc;
-    u32         _1ad0[(0x2750 - 0x1AD0) / sizeof(u32)];
+    u32                     _27c[(0x2A8 - 0x27C) / sizeof(u32)];
+    PlayerKey               mPlayerKey;
+    u32                     _33c[(0x490 - 0x33C) / sizeof(u32)];
+    u32                     mStatusArray[9];
+    u32                     _4b4[(0x1ACC - 0x4B4) / sizeof(u32)];
+    u32                     _1acc;
+    u32                     _1ad0[(0x219C - 0x1AD0) / sizeof(u32)];
+    FStateMgr<PlayerBase>   mStateMgr;
+    u32                     _21c0[(0x2750 - 0x21C0) / sizeof(u32)];
 };
 static_assert(sizeof(PlayerBase) == 0x2750);
