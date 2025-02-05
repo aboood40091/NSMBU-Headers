578c768f57d6b45f7a4cea2106a1abe547092975
Arulo <arulomodding@gmail.com>
2025-02-05 19:37:15 +0100

    Boss/BunDemoKamek: Add Adrresses

--- boss/BunDemoKamek.h
+++ boss/BunDemoKamek.h
@@ -2,16 +2,28 @@
 
 #include <enemy/Enemy.h>
 
-class BunDemoKameck : public Enemy {
-    SEAD_RTTI_OVERRIDE(BunDemoKameck, Enemy);
+class BunDemoKamek : public Enemy {//vtbl Address: 0x1000d274
+    SEAD_RTTI_OVERRIDE(BunDemoKamek, Enemy);
 
 public:
-    // Address: 0x0101F1bAC
-    DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState1);
-    // Address: 0x0101F1BCC
-    DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState2);
-    // Address: 0x0101F1bEC
-    DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState3);
-    // Address: 0x0101F1C0C
-    DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState4);
+    // BunDemoKamekState1                  Address: 0x101F1BAC
+    // initializeState_BunDemoKamekState1  Address: 0x02065A70
+    // executeState_BunDemoKamekState1     Address: 0x02065A74
+    // finalizeState_BunDemoKamekState1    Address: 0x02065A78
+    DECLARE_STATE_ID(BunDemoKamek, BunDemoKamekState1);
+    // BunDemoKamekState2                  Address: 0x0101F1BCC
+    // initializeState_BunDemoKamekState2  Address: 0x02064D60
+    // executeState_BunDemoKamekState2     Address: 0x02064E30
+    // finalizeState_BunDemoKamekState2    Address: 0x02064CF8
+    DECLARE_STATE_ID(BunDemoKamek, BunDemoKamekState2);
+    // BunDemoKamekState3                  Address: 0x101F1BEC
+    // initializeState_BunDemoKamekState3  Address: 0x02064FCC
+    // executeState_BunDemoKamekState3     Address: 0x02065124
+    // finalizeState_BunDemoKamekState3    Address: 0x02065A7C
+    DECLARE_STATE_ID(BunDemoKamek, BunDemoKamekState3);
+    // BunDemoKamekState4                  Address: 0x101F1C0C
+    // initializeState_BunDemoKamekState4  Address: 0x02065A80
+    // executeState_BunDemoKamekState4     Address: 0x02065A84
+    // finalizeState_BunDemoKamekState4    Address: 0x02065A88
+    DECLARE_STATE_ID(BunDemoKamek, BunDemoKamekState4);
 };
