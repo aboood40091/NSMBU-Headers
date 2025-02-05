20652ef01b6fa729cb997ca34b42682451391eb3
Arulo <arulomodding@gmail.com>
2025-02-05 18:24:06 +0100

    boss/BunKamekDemo: Fixed a typo

--- boss/BunDemoKamek.h
+++ boss/BunDemoKamek.h
@@ -6,8 +6,12 @@ class BunDemoKameck : public Enemy {
     SEAD_RTTI_OVERRIDE(BunDemoKameck, Enemy);
 
 public:
+    // Address: 0x0101F1bAC
     DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState1);
+    // Address: 0x0101F1BCC
     DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState2);
+    // Address: 0x0101F1bEC
     DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState3);
+    // Address: 0x0101F1C0C
     DECLARE_STATE_ID(BunDemoKameck, BunDemoKameckState4);
 };
