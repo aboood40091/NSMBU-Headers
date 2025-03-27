602098f7c989493acd49a830da318cbc0b10c51b
AboodXD <aboood40091@gmail.com>
2025-03-27 16:56:10 +0400

    input/SysControllerMgr: Add data members

--- input/SysControllerMgr.h
+++ input/SysControllerMgr.h
@@ -7,8 +7,29 @@ class SysControllerMgr
     SEAD_SINGLETON_DISPOSER(SysControllerMgr)
 
 public:
+    enum SetUpType
+    {
+        cSetUp_AB_XY = 0,
+        cSetUp_AX_BY
+    };
+    static_assert(sizeof(SetUpType) == 4);
+
+public:
     SysControllerMgr();
 
     void enterHBM();
     void exitHBM();
+
+    void setRemoteEnable(bool enable)
+    {
+        mIsRemoteEnable = enable;
+    }
+
+protected:
+    s32         mDRCTouchPanelIndex;
+    SetUpType   mSetUpType;
+    bool        mIsRemoteEnable;
+    u32         _1c[(0xB0 - 0x1C) / sizeof(u32)];
+
 };
+static_assert(sizeof(SysControllerMgr) == 0xB0);
