dc2821ef3240caa145298e0112953e6d464d024b
Arulo <arulomodding@gmail.com>
2025-05-07 10:37:45 +0200

    PlayerModelBase.h: Add some functions

---
+++ player/PlayerModelBase.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#include <heap/seadDisposer.h>
+
+
+class PlayerModelBase : public sead::IDisposer // vtbl Address: 0x1016BCE4 
+{
+    SEAD_RTTI_OVERRIDE(PlayerModelBase, sead::IDisposer);
+
+public:
+    // Address: 0x02920CA8
+    PlayerModelBase(const ActorCreateParam& param);
+
+public:
+
+    
+
+}
