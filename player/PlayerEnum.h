be0a5e95e29ab0543126534d149f0bfa721ec236
AboodXD <aboood40091@gmail.com>
2025-05-17 23:10:45 +0400

    player: Add PlayerMode & PlayerModelMode

---
+++ player/PlayerEnum.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+enum PlayerMode
+{
+    cPlayerMode_Small = 0,
+    cPlayerMode_Normal,
+    cPlayerMode_Fire,
+    cPlayerMode_Mini,
+    cPlayerMode_Propeller,
+    cPlayerMode_Penguin,
+    cPlayerMode_Ice,
+    cPlayerMode_Squirrel,
+    cPlayerMode_PSquirrel,
+    cPlayerMode_Num
+};
+
+enum PlayerModelMode
+{
+    cPlayerModelMode_Normal = 0,
+    cPlayerModelMode_Small,
+    cPlayerModelMode_Propeller,
+    cPlayerModelMode_Penguin,
+    cPlayerModelMode_Squirrel,
+    cPlayerModelMode_Num
+};
