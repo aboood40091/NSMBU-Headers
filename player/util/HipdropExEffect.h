dfaac7fe648e6051022ec22f548f15fd45447179
AboodXD <aboood40091@gmail.com>
2025-06-04 11:53:06 +0400

    player/util: Add (incomplete) HipdropExEffect

---
+++ player/util/HipdropExEffect.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <container/seadSafeArray.h>
+#include <math/seadVector.h>
+
+class AnimModel;
+
+class HipdropExEffect
+{
+public:
+    enum ModelType
+    {
+        cModelType_FB,
+        cModelType_Rainbow,
+        cModelType_Num
+    };
+    static_assert(cModelType_Num == 2);
+
+public:
+    // TODO
+
+//protected:
+    sead::SafeArray<AnimModel*, cModelType_Num> mModel;
+    sead::Vector3f                              mPosition;
+    bool                                        mIsEnable;
+};
+static_assert(sizeof(HipdropExEffect) == 0x18);
