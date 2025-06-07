ff734f3b38949b2aa6b88b7d71c57fc3a392d376
AboodXD <aboood40091@gmail.com>
2025-06-07 20:52:06 +0400

    collision/ActorCollisionTouchDrcCallback: Rename first virtual function

--- collision/ActorCollisionTouchDrcCallback.h
+++ collision/ActorCollisionTouchDrcCallback.h
@@ -10,7 +10,7 @@ class ActorCollisionTouchDrcCallback
 {
 public:
     // Address: 0x0219E7E8
-    virtual bool ccIsTouchSkipFrame(ActorCollisionCheck* p_cc, const sead::Vector2f& pos);
+    virtual bool ccSetTouchDamage(ActorCollisionCheck* p_cc, const sead::Vector2f& pos);
     virtual bool ccIsTouchEnable(ActorCollisionCheck* p_cc, const sead::Vector2f& pos) { return false; }
     // Address: 0x0219E8A4
     virtual void ccOnTouch(ActorCollisionCheck* p_cc, const sead::Vector2f& pos);
@@ -18,7 +18,7 @@ public:
     virtual void ccOnRelease(ActorCollisionCheck* p_cc) { }
 
     // Address: 0x0219E8B8
-    virtual bool bcIsTouchSkipFrame(BgCollision* p_bg_collision, const sead::Vector2f& pos);
+    virtual bool bcSetTouchDamage(BgCollision* p_bg_collision, const sead::Vector2f& pos);
     virtual bool bcIsTouchEnable(BgCollision* p_bg_collision, const sead::Vector2f& pos) { return false; }
     // Address: 0x0219E974
     virtual void bcOnTouch(BgCollision* p_bg_collision, const sead::Vector2f& pos);
