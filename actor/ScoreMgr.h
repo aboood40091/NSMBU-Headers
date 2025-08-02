784ee421535b3ea25a0726d3d58c0eeb65cef07a
AboodXD <aboood40091@gmail.com>
2025-08-03 00:06:11 +0400

    actor/ScoreMgr: Relabel score type enum values, add remaining score set functions

--- actor/ScoreMgr.h
+++ actor/ScoreMgr.h
@@ -18,34 +18,76 @@ class ScoreMgr
 public:
     enum ScoreType
     {
-        cScoreType_Point_100,
-        cScoreType_Point_200,
-        cScoreType_Point_400,
-        cScoreType_Point_800,
-        cScoreType_Point_1000,
-        cScoreType_Point_2000,
-        cScoreType_Point_4000,
-        cScoreType_Point_8000,
-        cScoreType_Rest_1,
-        cScoreType_Rest_3,
-        cScoreType_Unk,
-        cScoreType_Max
-    };
+        cScoreType_Invalid = -1,
 
-    enum ScoreTypeAll
-    {
-        cScoreTypeAll_Point_100,
-        cScoreTypeAll_Point_200,
-        cScoreTypeAll_Point_400,
-        cScoreTypeAll_Point_800,
-        cScoreTypeAll_Point_1000,
-        cScoreTypeAll_Point_2000,
-        cScoreTypeAll_Point_4000,
-        cScoreTypeAll_Point_8000,
-        cScoreTypeAll_Rest_All,
-        cScoreTypeAll_Point_10,
-        cScoreTypeAll_Max
+        /* Base */
+        cScoreType_Base_Combo_1 = 0,
+        cScoreType_Base_Combo_2,
+        cScoreType_Base_Combo_3,
+        cScoreType_Base_Combo_4,
+        cScoreType_Base_Combo_5,
+        cScoreType_Base_Combo_6,
+        cScoreType_Base_Combo_7,
+        cScoreType_Base_Combo_8,
+        cScoreType_Base_Combo_9,
+        cScoreType_Base_Num,
+
+        /* ScoreSet */
+
+        cScoreType_Point_100    = cScoreType_Base_Combo_1,
+        cScoreType_Point_200    = cScoreType_Base_Combo_2,
+        cScoreType_Point_400    = cScoreType_Base_Combo_3,
+        cScoreType_Point_800    = cScoreType_Base_Combo_4,
+        cScoreType_Point_1000   = cScoreType_Base_Combo_5,
+        cScoreType_Point_2000   = cScoreType_Base_Combo_6,
+        cScoreType_Point_4000   = cScoreType_Base_Combo_7,
+        cScoreType_Point_8000   = cScoreType_Base_Combo_8,
+        cScoreType_Rest_1       = cScoreType_Base_Combo_9,
+
+        cScoreType_Rest_3 = cScoreType_Base_Num,
+        cScoreType_Unk10,
+
+        cScoreType_Point_Num = cScoreType_Point_8000 + 1,
+        cScoreType_Num = cScoreType_Unk10 + 1,
+
+        /* UnKnownScoreSet */
+
+        cUnKnownScoreType_Point_100    = cScoreType_Base_Combo_1,
+        cUnKnownScoreType_Point_200    = cScoreType_Base_Combo_2,
+        cUnKnownScoreType_Point_400    = cScoreType_Base_Combo_3,
+        cUnKnownScoreType_Point_800    = cScoreType_Base_Combo_4,
+        cUnKnownScoreType_Point_1000   = cScoreType_Base_Combo_5,
+        cUnKnownScoreType_Point_2000   = cScoreType_Base_Combo_6,
+        cUnKnownScoreType_Point_4000   = cScoreType_Base_Combo_7,
+        cUnKnownScoreType_Point_8000   = cScoreType_Base_Combo_8,
+        cUnKnownScoreType_Rest_All     = cScoreType_Base_Combo_9,
+
+        cUnKnownScoreType_Point_10 = cScoreType_Base_Num,
+        cUnKnownScoreType_Num,
+
+        /* ScoreSet2 */
+    
+        cScoreType2_Point_1000_1    = cScoreType_Base_Combo_1,
+        cScoreType2_Point_1000_2    = cScoreType_Base_Combo_2,
+        cScoreType2_Point_2000      = cScoreType_Base_Combo_3,
+        cScoreType2_Point_4000      = cScoreType_Base_Combo_4,
+        cScoreType2_Point_8000      = cScoreType_Base_Combo_5,
+        cScoreType2_Rest_1_1        = cScoreType_Base_Combo_6,
+        cScoreType2_Rest_1_2        = cScoreType_Base_Combo_7,
+        cScoreType2_Rest_1_3        = cScoreType_Base_Combo_8,
+        cScoreType2_Rest_1_4        = cScoreType_Base_Combo_9,
+
+        cScoreType2_Rest_1_5 = cScoreType_Base_Num,
+
+        cScoreType2_Point_Num = cScoreType2_Point_8000 + 1,
+        cScoreType2_Num = cScoreType2_Rest_1_5 + 1
     };
+    static_assert(cScoreType_Point_Num == 8);
+    static_assert(cScoreType_Base_Num == 9);
+    static_assert(cScoreType_Num == 11);
+    static_assert(cUnKnownScoreType_Num == 10);
+    static_assert(cScoreType2_Point_Num == 5);
+    static_assert(cScoreType2_Num == 10);
 
 public:
     // Address: 0x020279E0
@@ -54,11 +96,17 @@ public:
     // Address: 0x02027BD8
     void incRestAll(bool sound);
     // Address: 0x02027C80
-    void addScore(const sead::Vector3f& small_score_pos, ScoreType type, s32 player_no, bool rest_sound = true);
+    void ScoreSet(const sead::Vector3f& small_score_pos, ScoreType type, s32 player_no, bool rest_sound = true);
+    // Address: 0x02027DA8
+    void ScoreSet(Actor* p_actor_for_small_score_pos, ScoreType type, s32 player_no, f32 small_score_offset_x = 0.0f, f32 small_score_offset_y = 24.0f);
+    // Address: 0x02027DF4
+    void ScoreSet2(const sead::Vector3f& small_score_pos, ScoreType type, s32 player_no);
+    // Address: 0x02027EBC
+    void ScoreSet2(Actor* p_actor_for_small_score_pos, ScoreType type, s32 player_no, f32 small_score_offset_x = 0.0f, f32 small_score_offset_y = 24.0f);
     // Address: 0x02027F04
-    void addScoreAll(const sead::Vector3f& small_score_pos, ScoreTypeAll type);
+    void UnKnownScoreCreate(const sead::Vector3f& small_score_pos, ScoreType type);
     // Address: 0x02027FCC
-    void addScoreAll(const Actor& actor_for_small_score_pos, f32 small_score_offset_x, f32 small_score_offset_y, ScoreTypeAll type);
+    void UnKnownScoreSet(Actor* p_actor_for_small_score_pos, ScoreType type, f32 small_score_offset_x = 0.0f, f32 small_score_offset_y = 24.0f);
     // Address: 0x0202821C
     void incRestAllWithSmallScore(bool sound);
 };
