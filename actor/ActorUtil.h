3b28dd74dde97ecfe2b4cf88121745c1d195aca8
AboodXD <aboood40091@gmail.com>
2025-08-08 19:57:51 +0400

    actor/ActorUtil: Add searchNearPlayer_Main

--- actor/ActorUtil.h
+++ actor/ActorUtil.h
@@ -7,6 +7,9 @@ class Actor;
 class ActorUtil
 {
 public:
+    // Address: 0x0200C67C
+    static s32 searchNearPlayer_Main(sead::Vector2f& player_pos, const sead::Vector2f& src_pos);
+
     // Address: 0x0200C7E4
     static u8 getTrgToSrcDir(const Actor& src, const Actor& trg);
     // Address: 0x0200C800
