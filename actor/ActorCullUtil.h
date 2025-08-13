e3098b55a84d183e703093f83bf5571ab1dd4d70
AboodXD <aboood40091@gmail.com>
2025-08-13 14:26:43 +0400

    actor/ActorCullUtil: Add screenCullCheck overload

--- actor/ActorCullUtil.h
+++ actor/ActorCullUtil.h
@@ -9,6 +9,8 @@ class ActorCullUtil
 public:
     // Address: 0x02008838
     static bool screenCullCheck(const sead::BoundBox2f& range, const ActorCullRange& cull_range);
+    // Address: 0x02008918
+    static bool screenCullCheck(const sead::BoundBox2f& range);
     // Address: 0x0200897C
     static bool areaCullCheck(const sead::BoundBox2f& range, u8 area_no);
 };
