d5bda1b62d99da6d38c8730cc41ce6d45abe2590
AboodXD <aboood40091@gmail.com>
2025-08-14 21:34:41 +0400

    game: Add CourseTask

---
+++ game/CourseTask.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <game/FieldGame.h>
+
+#include <framework/seadCalculateTask.h>
+#include <framework/seadTaskMgr.h>
+
+class CourseTask : public sead::CalculateTask   // vtbl Address: 0x100B734C
+{
+    // setInstance_()                               Address: 0x024BCA80
+    // deleteInstance()                             Address: Deleted
+    // sInstance                                    Address: 0x101D15F0
+    // SingletonDisposer_::~SingletonDisposer_()    Address: 0x024BC924
+    SEAD_TASK_SINGLETON_DISPOSER(CourseTask)
+
+public:
+    // Address: 0x024BCAA0
+    CourseTask(const sead::TaskConstructArg& arg);
+    // Address: 0x024BC950
+    virtual ~CourseTask();
+
+    const FieldGameData& getGameData() const
+    {
+        return mGameData;
+    }
+
+private:
+    bool            mCourseRestart;
+    u32             _cc[(0x124 - 0xCC) / sizeof(u32)];
+    FieldGameData   mGameData;
+    u32             _308[(0x5A4 - 0x308) / sizeof(u32)];
+};
+static_assert(sizeof(CourseTask) == 0x5A4);
