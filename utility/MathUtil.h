8e03d14ff8eb34bcabdf6947a440a03519d1f720
AboodXD <aboood40091@gmail.com>
2025-08-29 13:36:02 +0400

    utility: Add MathUtil (and move calcTimer to it)

--- utility/MathUtil.h
+++ utility/MathUtil.h
@@ -2,8 +2,15 @@
 
 #include <basis/seadTypes.h>
 
+class MathUtil
+{
+public:
+    template <typename T>
+    static T calcTimer(T* p_value);
+};
+
 template <typename T>
-T CalcTimer(T* p_value)
+T MathUtil::calcTimer(T* p_value)
 {
     if (*p_value != 0)
         (*p_value)--;
