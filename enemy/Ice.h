ba07184efb08f38c1a02b597d9f0a2736570ef8e
AboodXD <aboood40091@gmail.com>
2025-09-14 16:00:49 +0400

    enemy/Ice: Add RTTI

--- enemy/Ice.h
+++ enemy/Ice.h
@@ -4,6 +4,10 @@
 
 class Ice : public ActorState
 {
+    // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101E9CAC
+    // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101E9CB0
+    SEAD_RTTI_OVERRIDE(Ice, ActorState)
+
 public:
     // Address: 0x02372800
     void breakReq(s32 direction, s32 player_no);
