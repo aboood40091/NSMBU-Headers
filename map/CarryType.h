c91914b49f34844262e78b4aa2d122ee0f4e6654
AboodXD <aboood40091@gmail.com>
2025-11-03 23:13:22 +0400

    map: Add CarryType

---
+++ map/CarryType.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+enum CarryType
+{
+    cCarryType_LiftUp = 0,
+    cCarryType_Carry,
+    cCarryType_LiftUp_2,    // Not used by any object (IIRC)
+    cCarryType_Carry_2      // ^^^
+};
