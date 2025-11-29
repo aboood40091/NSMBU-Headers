1f6bf2dfec5bf9432518694f3c409dba481fa9e7
AboodXD <aboood40091@gmail.com>
2025-11-29 17:17:21 +0400

    actor/LineMgr: Update definition for init()

--- actor/LineMgr.h
+++ actor/LineMgr.h
@@ -2,6 +2,7 @@
 
 #include <state/FStateMgr.h>
 #include <utility/Angle.h>
+#include <utility/Direction.h>
 
 #include <container/seadSafeArray.h>
 #include <math/seadVector.h>
@@ -9,11 +10,18 @@
 class LineMgr
 {
 public:
+    enum Type
+    {
+        cType_ReverseAtEnd = 0, // i.e., Two-way
+        cType_StopAtEnd         // i.e., One-way
+    };
+
+public:
     // Address: 0x0200DC14
     LineMgr();
 
     // Address: 0x0200DF60
-    void initialize(const sead::Vector2f& pos, u8 layer, bool one_way, bool reverse_dir = false, f32 base_speed = 0.0f);
+    void init(const sead::Vector2f& pos, u8 layer, f32 base_speed = 0.0f, Type type = cType_ReverseAtEnd, u8 dir_x = cDirType_Right);
 
     // Address: 0x0200E0C0
     void setBaseSpeed(f32 speed);
@@ -41,9 +49,9 @@ protected:
     sead::Vector2f                          _7c;
     Angle                                   mAngle;
     f32                                     mBaseSpeed;
-    bool                                    mOneWay;
+    u8                                      mType;          // See Type
     u8                                      mLayer;
-    bool                                    mReverseDir;
+    u8                                      mDir;
     bool                                    _8f;            // Set, but never read
 };
 static_assert(sizeof(LineMgr) == 0x90);
