f7e112e2d95ac07e16fb0ce3a4aee7c27ed07b6f
AboodXD <aboood40091@gmail.com>
2025-11-29 17:19:23 +0400

    player/PlayerMgr: Define remaining data members

--- player/PlayerMgr.h
+++ player/PlayerMgr.h
@@ -3,6 +3,7 @@
 #include <container/seadRingBuffer.h>
 #include <container/seadSafeArray.h>
 #include <heap/seadDisposer.h>
+#include <math/seadVector.h>
 #include <prim/seadBitFlag.h>
 
 class PlayerObject;
@@ -11,6 +12,14 @@ class Yoshi;
 class PlayerMgr
 {
     SEAD_SINGLETON_DISPOSER(PlayerMgr)
+
+public:
+    enum RDashPhysicsType
+    {
+        cRDashPhysics_Luigi = 0,
+        cRDashPhysics_Mario
+    };
+    static_assert(sizeof(RDashPhysicsType) == 4);
 
 public:
     PlayerObject* getPlayerObject(s32 player_no)
@@ -25,22 +34,27 @@ public:
 
     bool isPlayerActive(s32 player_no)
     {
-        return mPlayerFlag.isOnBit(player_no);
+        return mActPlayerInfo.isOnBit(player_no);
     }
 
     s32 getNum()
     {
-        return mPlayerFlag.countOnBit();
+        return mActPlayerInfo.countOnBit();
     }
 
     void addNum(s32 player_no)
     {
-        mPlayerFlag.setBit(player_no);
+        mActPlayerInfo.setBit(player_no);
     }
 
     void decNum(s32 player_no)
     {
-        mPlayerFlag.resetBit(player_no);
+        mActPlayerInfo.resetBit(player_no);
+    }
+
+    s32 getCannonJumpTimer() const
+    {
+        return mCannonJumpTimer;
     }
 
     bool isAcceptQuake(s32 player_no);
@@ -52,11 +66,30 @@ public:
 private:
     u32                                 _10[(0x20 - 0x10) / sizeof(u32)];
     sead::SafeArray<PlayerObject*, 4>   mPlayerObject;
-    sead::SafeArray<PlayerObject*, 4>   mPlayerObject2; // Idk why
-    sead::BitFlag8                      _40;
-    sead::BitFlag16                     mPlayerFlag;
+    sead::SafeArray<PlayerObject*, 4>   mPlayerObject2;     // Idk why
+    sead::BitFlag8                      mCreateFlag;
+    sead::BitFlag16                     mActPlayerInfo;
     sead::FixedRingBuffer<Yoshi*, 4>    mYoshi;
     sead::FixedRingBuffer<Yoshi*, 4>    mYoshi2;
-    u32                                 _84[(0xD0 - 0x84) / sizeof(u32)];
+    u32                                 _84;
+    u8                                  _88;
+    sead::BitFlag32                     mPauseDisable;      // Maybe?
+    sead::BitFlag32                     mPauseDisableInfo;  // Maybe?
+    bool                                mDisableOrchestra;
+    sead::BitFlag32                     mCreateBalloonFlag;
+    bool                                mAllBalloon;
+    sead::BitFlag8                      mCannonJumpFlag;
+    s32                                 mCannonJumpTimer;
+    sead::BitFlag16                     mBgmState;
+    u32                                 _a8;
+    s32                                 mPreFanfareTimer;
+    bool                                mOdaiClear;
+    u8                                  _b1;
+    s32                                 mHpDpSpecialType;
+    s32                                 mCoinMaxNum;
+    sead::Vector3f                      mPlayerSetPos;
+    sead::BitFlag16                     _c8;
+    sead::BitFlag16                     _ca;
+    RDashPhysicsType                    mRDashPhysics;
 };
 static_assert(sizeof(PlayerMgr) == 0xD0);
