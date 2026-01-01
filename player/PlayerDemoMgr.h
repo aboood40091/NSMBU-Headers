af32b293306f7c6f62affb4f093019032d113100
AboodXD <aboood40091@gmail.com>
2026-01-01 20:06:46 +0400

    player/PlayerDemoMgr: Add course-out-related functions

--- player/PlayerDemoMgr.h
+++ player/PlayerDemoMgr.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <container/seadRingBuffer.h>
 #include <heap/seadDisposer.h>
 
 class PlayerDemoMgr
@@ -8,4 +9,23 @@ class PlayerDemoMgr
 
 public:
     bool isPlayerGameStop();
+
+    void setCourseOutList(s32 player_no);
+
+    s32 getCourseOutList(s32 index) const
+    {
+        return mCourseOutList[index];
+    }
+
+    void setCourseOutPlayerNo(s32 player_no)
+    {
+        mCourseOutPlayerNo = player_no;
+    }
+protected:
+    u32                             _10[(0x158 - 0x10) / sizeof(u32)];
+    sead::FixedRingBuffer<s32, 4>   mCourseOutList;
+    u32                             _178[(0x1B8 - 0x178) / sizeof(u32)];
+    s32                             mCourseOutPlayerNo;
+    u32                             _1BC[(0x1F4 - 0x1BC) / sizeof(u32)];
 };
+static_assert(sizeof(PlayerDemoMgr) == 0x1F4);
