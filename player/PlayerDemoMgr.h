7a02093cdd0cab0f0e3667280a39e84b6558137d
AboodXD <aboood40091@gmail.com>
2026-01-04 13:47:55 +0400

    player/PlayerDemoMgr: Add remaining DemoNo-related functions

--- player/PlayerDemoMgr.h
+++ player/PlayerDemoMgr.h
@@ -24,13 +24,29 @@ public:
         mCourseOutPlayerNo = player_no;
     }
 
-    void clearDemoNo(s8 player_no);
+    bool isEnableCheckDemoNo()
+    {
+        return mIsEnableCheckDemoNo;
+    }
+
+    void setEnableCheckDemoNo()
+    {
+        mIsEnableCheckDemoNo = true;
+    }
+
+    s32 getNextDemoNo();
+    s32 getNumDemoNo();
+    void turnNextDemoNo();
+    void clearDemoNo(s32 player_no);
+    s32 searchDemoNo(s32 player_no);
 
 protected:
     u32                             _10[(0x158 - 0x10) / sizeof(u32)];
     sead::FixedRingBuffer<s32, 4>   mCourseOutList;
     u32                             _178[(0x1B8 - 0x178) / sizeof(u32)];
     s32                             mCourseOutPlayerNo;
-    u32                             _1BC[(0x1F4 - 0x1BC) / sizeof(u32)];
+    u32                             _1bc;
+    bool                            mIsEnableCheckDemoNo;
+    u32                             _1c4[(0x1F4 - 0x1C4) / sizeof(u32)];
 };
 static_assert(sizeof(PlayerDemoMgr) == 0x1F4);
