7c5ac3bce43402ac30926efca47894530d5199a8
AboodXD <aboood40091@gmail.com>
2026-01-07 20:46:29 +0400

    event: Add EventDemoNextGotoBlock

---
+++ event/EventDemoNextGotoBlock.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <event/EventBase.h>
+
+class EventDemoNextGotoBlock : public EventBase // vtbl Address: 0x100B44F0
+{
+    // getRuntimeTypeInfoStatic()::typeInfo initialization guard variable   Address: 0x101EAD00
+    // getRuntimeTypeInfoStatic()::typeInfo                                 Address: 0x101EAD04
+    SEAD_RTTI_OVERRIDE(EventDemoNextGotoBlock, EventBase)
+};
