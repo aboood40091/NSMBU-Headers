a6354897625d7bb1c60e0b263ab5b54ca8d5bfcd
AboodXD <aboood40091@gmail.com>
2026-02-13 18:57:59 +0400

    player: Add PlayerHIO_Character

---
+++ player/PlayerHIO_Character.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <player/PlayerEnum.h>
+
+// Address: 0x029121C8
+f32 GetCarryMiniPlayerAddYOffs(PlayerCharacter character, PlayerMode mode);
+// Address: 0x029121D8
+f32 GetCarrySmallPlayerAddYOffs(PlayerCharacter character, PlayerMode mode);
