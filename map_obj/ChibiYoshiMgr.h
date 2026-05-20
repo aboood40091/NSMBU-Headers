4962c2a45ebb88b6bfc43cd850aa5daf2e49e012
AboodXD <aboood40091@gmail.com>
2026-05-20 19:34:37 +0400

    map_obj/ChibiYoshiMgr: Fix signature for spawnEgg

--- map_obj/ChibiYoshiMgr.h
+++ map_obj/ChibiYoshiMgr.h
@@ -8,5 +8,7 @@ class ChibiYoshiMgr
 {
 public:
     // Address: 0x02743BB0
-    static void spawnEgg(const sead::Vector3f& pos, u8 type, DirType dir_type);
+    // type: 0 = bubble, 1 = balloon, 2 = light
+    // spawn_type: See BlockCoinBase::VSpawnType
+    static void spawnEgg(const sead::Vector3f& pos, u8 type, u32 spawn_type);
 };
