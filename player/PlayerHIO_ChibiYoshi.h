1e33ec31d18568c09e57a24d708f982379c5e76d
AboodXD <aboood40091@gmail.com>
2026-06-03 12:06:15 +0400

    player: Add PlayerHIO_ChibiYoshi

---
+++ player/PlayerHIO_ChibiYoshi.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+// Address: 0x1016A720
+extern const f32 cBalloonChibiYoshiFlyAccelF;               // 0.02f
+// Address: 0x1016A724
+extern const f32 cBalloonChibiYoshiFlyMaxSpeedF;            // 1.5f
+
+// Address: 0x1016a728
+extern const f32 cBalloonChibiYoshiFlyInitSpeedY;           // 2.3f
+// Address: 0x1016a72C
+extern const f32 cBalloonChibiYoshiFlyInitSpeedYMax;        // 3.0f
+// Address: 0x1016A730
+extern const f32 cBalloonChibiYoshiFlySmallInitSpeedYDecel; // 0.6f
+
+// Address: 0x1016A734
+extern const f32 cBalloonChibiYoshiFlySpinSpeedYMax;        // 3.0f
+// Address: 0x1016A738
+extern const f32 cBalloonChibiYoshiFlySpinAddSpeedY;        // 4.0f
+// Address: 0x1016A73C
+extern const f32 cBalloonChibiYoshiFlySpinTrigSpeedY;       // 0.3f
+
+// Address: 0x1016A740
+extern const s32 cBalloonChibiYoshiFlyAscendMinTime;        // 5 frames
+
+// Address: 0x1016A744
+extern const f32 cBalloonChibiYoshiFlyDescendStartSpeedY;   // 0.0f
+// Address: 0x1016A748
+extern const f32 cBalloonChibiYoshiFlyAscendEndGravity;     // -0.03f
+// Address: 0x1016A74C
+extern const f32 cBalloonChibiYoshiFlyDescendGravity;       // -0.03f
+
+// Address: 0x1016A750
+extern const f32 cBalloonChibiYoshiFlyMaxFallSpeed;         // -1.5f
+
+// Address: 0x1016A754
+extern const s32 cBalloonChibiYoshiFlyAscendGravityTime;    // 10 frames
+// Address: 0x1016A758
+extern const f32 cBalloonChibiYoshiFlyAscendGravity[2];     // { -0.2f, -0.34f }
