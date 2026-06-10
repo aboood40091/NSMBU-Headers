64b84e218bea3f2689041dae986b858737bcda38
AboodXD <aboood40091@gmail.com>
2026-06-10 09:09:18 +0400

    map_obj/ChibiYoshiBase: Add setCannonJump

--- map_obj/ChibiYoshiBase.h
+++ map_obj/ChibiYoshiBase.h
@@ -9,6 +9,10 @@ class ChibiYoshiBase : public ActorCollision    // vtbl Address: 0x101084FC
     SEAD_RTTI_OVERRIDE(ChibiYoshiBase, ActorCollision)
 
 public:
+    // Address: 0x027317BC
+    void setCannonJump(const sead::Vector3f& pos, const sead::Vector2f& speed, s32 jump_time);
+
+public:
     u32 _17a0[(0x1A78 - 0x17A0) / sizeof(u32)];
     u32 _1a78;
     u32 _1a7c[(0x1F48 - 0x1A7C) / sizeof(u32)];
